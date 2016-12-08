fdd81a99e3f7e67a25b204e266153eec030a0530
sshpark <huangjiamingwl@gmail.com>
2016-12-08 14:39:34 +0800

    first masm

---
+++ 1.asm
@@ -0,0 +1,28 @@
+DSEG SEGMENT
+	VAR1 DB 0FFH		;定义第一个数据
+	VAR2 DB 0FFH		;定义第二个数据
+	VAR3 DD ?			;定义一个双字变量
+DSEG ENDS
+
+CSEG SEGMENT
+	ASSUME CS:CSEG, DS:DSEG
+	START:
+		MOV AX, DSEG
+		MOV DS, AX
+		MOV AH, VAR1
+		MOV AL, AH
+		MUL AH
+		MOV DX, AX
+		MOV AL, VAR2
+		MOV AH, AL
+		MUL AH				;进行al * ah 的操作，求 y 的平方
+		ADD DX, AX			;进行 x2 + y2 的操作
+		MOV WORD PTR VAR3, DX			;将和送入var3变量的低字、
+		MOV DX, 0000H
+		ADC DX, 0000H					;得到进位
+		MOV WORD PTR VAR3 + 2, DX
+		MOV 4CH
+		INT 21H
+CSEG ENDS
+END START
+		
